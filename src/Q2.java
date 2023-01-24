f8e24fb7a07e514efa79749eeef90dc972d3c73e
Manan <kharwarm@mcmaster.ca>
2023-01-23 19:11:11 -0500

    Q2 Added

--- src/Q2.java
+++ src/Q2.java
@@ -1,6 +1,19 @@
-public class Q2 {
+import java.util.*;
 
-    public static String rearrangeWord(String word){
+public class Q2 {
+    public static Set<String> set = new HashSet<>();
+    public static String swap(String word, int start, int end){
+        char[] back = word.toCharArray();
+        char ch;
+
+        ch = back[start];
+        back[start] = back[end];
+        back[end] = ch;
+
+        return  String.valueOf(back);
+    }
+
+    public static Set<String> rearrangeWord(String word, int start, int end){
         // what I was thinking is finding all the permutations of a string and then just having a loop to find out the next big permutation
         // turns out I'm running into TLE -> time limit exceeded
 
@@ -8,15 +21,32 @@ public class Q2 {
         // 3 -> 6 xyz [xyz, xzy, yxz, yzx, zyx, zyx]
         // 4 -> 24 wxyz
 
+        if(word.length() == 0){
+            return set;
+        }
+        // first find how to calculate all the permutations of the string
+        for(int i = start; i < end; i++){
+            // using substring
 
+            word = swap(word, start, i);
+            set.add(word);
+            rearrangeWord(word, start + 1, end);
+            word = swap(word, start, i);
+            set.add(word);
 
-        return "";
+        }
+
+        //System.out.println(set);
+        return set;
     }
 
     public static void main(String[] args){
         // Rearranging the word
         // We have to find the next permutation of the string. If there are no next permutation simply return "no answer"
         String word = "baca";
-        String result = rearrangeWord(word);
+        set = rearrangeWord(word, 0, word.length());
+        List<String> result = new ArrayList<>(set);
+        Collections.sort(result);
+        System.out.println(result);
     }
 }
